b697ff4537ba2b2526e8f93eaa7d3788538059a7
Clyde Fare <clyde.fare@gmail.com>
2014-01-24 21:50:21 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='pbs_util',
+      version='0.1',
+      author='Paul J. Ledbetter',
+      author_email='plediii@github.com',
+      url='https://github.com/plediii/pbs_util',
+      packages=['pbs_util','pbs_util.tempfile_util'],
+      )
