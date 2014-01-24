1c1ed93773a9a020f9216056d2ae49cc0cd589d1
Clyde Fare <clyde.fare@gmail.com>
2014-01-24 23:40:29 +0000

    Altered setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ setup(name='pbs_util',
       version='0.1',
       author='Paul J. Ledbetter',
       author_email='plediii@github.com',
-      url='https://github.com/Clyde_fare/pbs_util',
+      url='https://github.com/plediii/pbs_util',
       packages=['pbs_util'],
       )
