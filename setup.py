a6e30551c65ab55809770ba4a05ebeb4ef6bfd73
Clyde Fare <clyde.fare@gmail.com>
2014-01-24 23:24:08 +0000

    Modified structure and created setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='pbs_util',
+      version='0.1',
+      author='Paul J. Ledbetter',
+      author_email='plediii@github.com',
+      url='https://github.com/Clyde_fare/pbs_util',
+      packages=['pbs_util'],
+      )
